fc654475127a0ba658260196479feb21fb8eb3ac
Scheleder <scheleder@yahoo.com>
2023-01-23 21:48:16 -0300

    add Service Seller

---
+++ SalesWebMVC/Controllers/SellersActionsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Controllers
+{
+    public class SellersActionsController : Controller
+    {
+        public readonly SellerService _service;
+
+        public SellersActionsController(SellerService service)
+        {
+            _service = service;
+        }
+
+        public IActionResult Index()
+        {
+            var list = _service.FindAll();
+            return View(list);
+        }
+    }
+}
